6d63321ebfe773d271a32ee435dc31482265beaa
João Santos <jonaspauleta2@gmail.com>
2022-08-05 15:48:27 +0100

    email verification, migrations, models, comminity controller, community request, community routes, register input errors

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CommunityController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -24,8 +25,11 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::group(['middleware' => ['auth', 'verified']], function () {
+    Route::inertia('/dashboard', 'Dashboard')->name('dashboard');
+
+    Route::resource('/communities', CommunityController::class);
+
+});
 
 require __DIR__.'/auth.php';
